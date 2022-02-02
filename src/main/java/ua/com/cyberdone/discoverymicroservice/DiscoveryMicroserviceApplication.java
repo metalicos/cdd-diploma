f5fad09f4d2dcb8bd8ea01c816c52a68126a50dd
Ostap Komplikevych <ostap.ja@gmail.com>
2022-02-02 15:36:04 +0200

    first commit

---
+++ src/main/java/ua/com/cyberdone/discoverymicroservice/DiscoveryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ua.com.cyberdone.discoverymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryMicroserviceApplication.class, args);
+    }
+
+}
