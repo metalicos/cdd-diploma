05e4d6b6c578d1b30c389f346651796a1ce98d68
Ostap Komplikevych <ostap.ja@gmail.com>
2022-02-02 15:59:50 +0200

    [cdd-01] fixed annotation on test

--- src/main/java/ua/com/cyberdone/discoverymicroservice/DiscoveryMicroserviceApplication.java
+++ src/main/java/ua/com/cyberdone/discoverymicroservice/DiscoveryMicroserviceApplication.java
@@ -2,7 +2,9 @@ package ua.com.cyberdone.discoverymicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryMicroserviceApplication {
 
